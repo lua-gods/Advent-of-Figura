81f1561dc79554bbcdfc528b78fff1fe60b09caa
4P5 <theprismarinescientist@gmail.com>
2023-11-12 18:28:50 +1300

    Better wheel
    - Wheel now loads all files in the `days` directory and creates a head for each, using the new texture storage method

--- modules/wheel.lua
+++ modules/wheel.lua
@@ -1,28 +1,38 @@
 if not IS_HOST then return end
 
+local base64 = require("libraries.base64")
+
 local main_page = action_wheel:newPage()
 action_wheel:setPage(main_page)
 
-local head = "player_head{SkullOwner:" .. avatar:getEntityName() .. "}"
-main_page:newAction():title("Head"):item(head):onLeftClick(function ()
-    host:setSlot(player:getNbt().SelectedItemSlot, head)
-    sounds["entity.item.pickup"]:pos(player:getPos()):play()
-end)
-
-local function dayItem(count)
-    return world.newItem(count == -1 and "barrier" or "clock", math.abs(count), 5)
+local function giveHead(name)
+    local item = world.newItem("player_head" .. (toJson{
+        SkullOwner = {
+            Id = {
+                client.uuidToIntArray(avatar:getUUID())
+            },
+            Properties = {
+                textures = {
+                    {
+                        Value = base64.encode(name)
+                    }
+                }
+            }
+        }
+    }):gsub('"Id":%[','"Id":[I;'), tonumber(name))
+    main_page:newAction():title(name):item(item):onLeftClick(function ()
+        host:setSlot(player:getNbt().SelectedItemSlot, item)
+        sounds["entity.item.pickup"]:pos(player:getPos()):play()
+    end)
 end
-main_page:newAction():title("Override day (scroll)"):item(dayItem(-1)):onScroll(function (dir, self)
-    if not self:isToggled() then return end
-    DATE = (DATE or 1) + dir
-    self:setItem(dayItem(DATE))
-end):onToggle(function (state, self)
-    if state then
-        DATE = 1
-        self:setItem(dayItem(DATE))
-    else
-        DATE = nil
-        self:setItem(dayItem(-1))
-    end
-end)
 
+local n_actions = 0
+for _, file in next, listFiles("days") do
+    n_actions = n_actions + 1
+    giveHead(file:gsub("days%.",""))
+end
+
+while n_actions % 8 ~= 0 do
+    n_actions = n_actions + 1
+    main_page:newAction():hoverColor(0,0,0)
+end
