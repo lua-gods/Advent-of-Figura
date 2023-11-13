8fc2d410431628b0294537deb443bd10fe086d99
4P5 <theprismarinescientist@gmail.com>
2023-11-13 19:43:10 +1300

    Snowman improvements
    - Fixed a bug where bottom variants showed up in the middle
    - Made random more random by generating more random every random
    - Added a hint when placing a single head to place more (Tween lib is awesome!)

--- days/snowman.lua
+++ days/snowman.lua
@@ -1,5 +1,6 @@
 local Calendar = require("libraries.Calendar")
 local manager = require("libraries.SkullManager")
+local tween = require("libraries.GNTweenLib")
 
 local day = Calendar:newDay("snowman", 3)
 
@@ -23,18 +24,21 @@ end
 
 local function roll(skull)
     skull.renderer:reset()
-    local type = "top"
-    if world.getBlockState(skull.pos + vec(0,1,0)).id:find("head") then
+    local block_above = world.getBlockState(skull.pos + vec(0,1,0))
+    local block_below = world.getBlockState(skull.pos + vec(0,-1,0))
+    
+    local type = "middle"
+    if block_above:isAir() then
+        type = "top"
+    elseif not block_below.id:find("head") then
         type = "bottom"
-        if world.getBlockState(skull.pos + vec(0,-1,0)).id:find("head") then
-            type = "middle"
-        end
     end
 
     local top_variant = variants[type]
-    local bottom_variant = top_variant == variants.middle and variants.bottom or variants.middle
+    local bottom_variant = not block_below.id:find("head") and variants.bottom or variants.middle
 
     math.randomseed(skull.pos.x * 73856093 + skull.pos.y * 19349663 + skull.pos.z * 83492791)
+    math.random(); math.random(); math.random()
     local top = skull:addPart(top_variant[math.random(1, #top_variant)])
     if type ~= "top" then
         top:rot(0, rng.float(0, 360), 0)
@@ -47,6 +51,22 @@ local function roll(skull)
         processVariant(bottom)
         bottom:setPos(bottom:getPos() - vec(0, 8, 0))
     end
+end
+
+local function hint(skull)
+    local hint_model = skull:addPart(models.snowman.Bottom.basic)
+    hint_model:pos(skull.render_pos * 16 + vec(0, 8.01, 0)):primaryRenderType("TRANSLUCENT")
+
+    tween.tweenFunction(0, 1, 1, "outQuad", function(x)
+        hint_model:opacity(x):scale(x * 0.05 + 0.95)
+    end, function()
+        tween.tweenFunction(1, 0, 1, "outQuad", function(x)
+            hint_model:opacity(x):scale(x * 0.05 + 0.95)
+        end, function()
+            if not hint_model:getParent() then return end
+            hint_model:getParent():removeChild(hint_model)
+        end)
+    end)
 end
 
 function day:init(skull)
@@ -62,6 +82,13 @@ function day:init(skull)
         end
     end
     skull.data.ripple()
+
+    local block_above = world.getBlockState(skull.pos + vec(0,1,0))
+    local block_below = world.getBlockState(skull.pos + vec(0,-1,0))
+
+    if block_above:isAir() and not block_below.id:find("head") then
+        hint(skull)
+    end
 end
 
 function day:exit(skull)
