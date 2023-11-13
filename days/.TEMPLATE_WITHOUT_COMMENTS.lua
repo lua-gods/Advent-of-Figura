62ea16fb7f6e09ceba9b5a6ed83f657e3de2fc3b
4P5 <theprismarinescientist@gmail.com>
2023-11-13 17:52:39 +1300

    Blank template
    - A template without comments or other code

---
+++ days/.TEMPLATE_WITHOUT_COMMENTS.lua
@@ -0,0 +1,28 @@
+local Calendar = require("libraries.Calendar")
+local day = Calendar:newDay("", -1)
+
+---@param skull Skull
+function day:init(skull)
+
+end
+
+---@param skull Skull
+function day:tick(skull)
+
+end
+
+---@param skull Skull
+function day:punch(skull)
+
+end
+
+---@param skull Skull
+---@param delta number
+function day:render(skull, delta)
+
+end
+
+---@param skull Skull
+function day:exit(skull)
+
+end
