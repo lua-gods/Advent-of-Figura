1f7e1f352663fa2f5e3c7aa06ad4b5e1efe10a04
GNamimates <81744903+GNamimates@users.noreply.github.com>
2023-11-13 18:45:21 +0800

    tween fixes

--- libraries/GNTweenLib.lua
+++ libraries/GNTweenLib.lua
@@ -3,6 +3,7 @@
  / / __/  |/ /
 / /_/ / /|  /
 \____/_/ |_/ ]] --[[
+--[[
 Disclaimer for Robert Penner's Easing Equations license:
 
 TERMS OF USE - EASING EQUATIONS
@@ -14,24 +15,34 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-   * Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]] ---@diagnostic disable: assign-type-mismatch
-local po = math.pow
-local si = math.sin
-local co = math.cos
-local pi = math.pi
-local sq = math.sqrt
-local ab = math.abs
-local as = math.asin
+]]
 
-local function linear(t, b, c, d) return c * t / d + b end
+-- For all easing functions:
+-- t = elapsed time
+-- b = begin
+-- c = change == ending - beginning
+-- d = duration (total time)
+
+local pow = math.pow
+local sin = math.sin
+local cos = math.cos
+local pi = math.pi
+local sqrt = math.sqrt
+local abs = math.abs
+local asin  = math.asin
+
+local function linear(t, b, c, d)
+  return c * t / d + b
+end
+
 local function inQuad(t, b, c, d)
   t = t / d
-  return c * po(t, 2) + b
+  return c * pow(t, 2) + b
 end
 
 local function outQuad(t, b, c, d)
@@ -42,7 +53,7 @@ end
 local function inOutQuad(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * po(t, 2) + b
+    return c / 2 * pow(t, 2) + b
   else
     return -c / 2 * ((t - 1) * (t - 3) - 1) + b
   end
@@ -50,20 +61,20 @@ end
 
 local function outInQuad(t, b, c, d)
   if t < d / 2 then
-    return outQuad(t * 2, b, c / 2, d)
+    return outQuad (t * 2, b, c / 2, d)
   else
     return inQuad((t * 2) - d, b + c / 2, c / 2, d)
   end
 end
 
-local function inCubic(t, b, c, d)
+local function inCubic (t, b, c, d)
   t = t / d
-  return c * po(t, 3) + b
+  return c * pow(t, 3) + b
 end
 
 local function outCubic(t, b, c, d)
   t = t / d - 1
-  return c * (po(t, 3) + 1) + b
+  return c * (pow(t, 3) + 1) + b
 end
 
 local function inOutCubic(t, b, c, d)
@@ -86,21 +97,21 @@ end
 
 local function inQuart(t, b, c, d)
   t = t / d
-  return c * po(t, 4) + b
+  return c * pow(t, 4) + b
 end
 
 local function outQuart(t, b, c, d)
   t = t / d - 1
-  return -c * (po(t, 4) - 1) + b
+  return -c * (pow(t, 4) - 1) + b
 end
 
 local function inOutQuart(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * po(t, 4) + b
+    return c / 2 * pow(t, 4) + b
   else
     t = t - 2
-    return -c / 2 * (po(t, 4) - 2) + b
+    return -c / 2 * (pow(t, 4) - 2) + b
   end
 end
 
@@ -114,21 +125,21 @@ end
 
 local function inQuint(t, b, c, d)
   t = t / d
-  return c * po(t, 5) + b
+  return c * pow(t, 5) + b
 end
 
 local function outQuint(t, b, c, d)
   t = t / d - 1
-  return c * (po(t, 5) + 1) + b
+  return c * (pow(t, 5) + 1) + b
 end
 
 local function inOutQuint(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return c / 2 * po(t, 5) + b
+    return c / 2 * pow(t, 5) + b
   else
     t = t - 2
-    return c / 2 * (po(t, 5) + 2) + b
+    return c / 2 * (pow(t, 5) + 2) + b
   end
 end
 
@@ -140,17 +151,23 @@ local function outInQuint(t, b, c, d)
   end
 end
 
-local function inSine(t, b, c, d) return -c * c(t / d * (pi / 2)) + c + b end
+local function inSine(t, b, c, d)
+  return -c * cos(t / d * (pi / 2)) + c + b
+end
 
-local function outSine(t, b, c, d) return c * si(t / d * (pi / 2)) + b end
+local function outSine(t, b, c, d)
+  return c * sin(t / d * (pi / 2)) + b
+end
 
-local function inOutSine(t, b, c, d) return -c / 2 * (c(pi * t / d) - 1) + b end
+local function inOutSine(t, b, c, d)
+  return -c / 2 * (cos(pi * t / d) - 1) + b
+end
 
 local function outInSine(t, b, c, d)
   if t < d / 2 then
     return outSine(t * 2, b, c / 2, d)
   else
-    return inSine((t * 2) - d, b + c / 2, c / 2, d)
+    return inSine((t * 2) -d, b + c / 2, c / 2, d)
   end
 end
 
@@ -158,7 +175,7 @@ local function inExpo(t, b, c, d)
   if t == 0 then
     return b
   else
-    return c * po(2, 10 * (t / d - 1)) + b - c * 0.001
+    return c * pow(2, 10 * (t / d - 1)) + b - c * 0.001
   end
 end
 
@@ -166,7 +183,7 @@ local function outExpo(t, b, c, d)
   if t == d then
     return b + c
   else
-    return c * 1.001 * (-po(2, -10 * t / d) + 1) + b
+    return c * 1.001 * (-pow(2, -10 * t / d) + 1) + b
   end
 end
 
@@ -175,10 +192,10 @@ local function inOutExpo(t, b, c, d)
   if t == d then return b + c end
   t = t / d * 2
   if t < 1 then
-    return c / 2 * po(2, 10 * (t - 1)) + b - c * 0.0005
+    return c / 2 * pow(2, 10 * (t - 1)) + b - c * 0.0005
   else
     t = t - 1
-    return c / 2 * 1.0005 * (-po(2, -10 * t) + 2) + b
+    return c / 2 * 1.0005 * (-pow(2, -10 * t) + 2) + b
   end
 end
 
@@ -192,21 +209,21 @@ end
 
 local function inCirc(t, b, c, d)
   t = t / d
-  return (-c * (sq(1 - po(t, 2)) - 1) + b)
+  return(-c * (sqrt(1 - pow(t, 2)) - 1) + b)
 end
 
 local function outCirc(t, b, c, d)
   t = t / d - 1
-  return (c * sq(1 - po(t, 2)) + b)
+  return(c * sqrt(1 - pow(t, 2)) + b)
 end
 
 local function inOutCirc(t, b, c, d)
   t = t / d * 2
   if t < 1 then
-    return -c / 2 * (sq(1 - t * t) - 1) + b
+    return -c / 2 * (sqrt(1 - t * t) - 1) + b
   else
     t = t - 2
-    return c / 2 * (sq(1 - t * t) + 1) + b
+    return c / 2 * (sqrt(1 - t * t) + 1) + b
   end
 end
 
@@ -223,24 +240,26 @@ local function inElastic(t, b, c, d, a, p)
 
   t = t / d
 
-  if t == 1 then return b + c end
+  if t == 1  then return b + c end
 
   if not p then p = d * 0.3 end
 
   local s
 
-  if not a or a < a(c) then
+  if not a or a < abs(c) then
     a = c
     s = p / 4
   else
-    s = p / (2 * pi) * as(c / a)
+    s = p / (2 * pi) * asin(c/a)
   end
 
   t = t - 1
 
-  return -(a * p(2, 10 * t) * s((t * d - s) * (2 * pi) / p)) + b
+  return -(a * pow(2, 10 * t) * sin((t * d - s) * (2 * pi) / p)) + b
 end
 
+-- a: amplitud
+-- p: period
 local function outElastic(t, b, c, d, a, p)
   if t == 0 then return b end
 
@@ -252,16 +271,18 @@ local function outElastic(t, b, c, d, a, p)
 
   local s
 
-  if not a or a < a(c) then
+  if not a or a < abs(c) then
     a = c
     s = p / 4
   else
-    s = p / (2 * pi) * as(c / a)
+    s = p / (2 * pi) * asin(c/a)
   end
 
-  return a * p(2, -10 * t) * s((t * d - s) * (2 * pi) / p) + c + b
+  return a * pow(2, -10 * t) * sin((t * d - s) * (2 * pi) / p) + c + b
 end
 
+-- p = period
+-- a = amplitud
 local function inOutElastic(t, b, c, d, a, p)
   if t == 0 then return b end
 
@@ -274,19 +295,19 @@ local function inOutElastic(t, b, c, d, a, p)
 
   local s
 
-  if not a or a < a(c) then
+  if not a or a < abs(c) then
     a = c
     s = p / 4
   else
-    s = p / (2 * pi) * as(c / a)
+    s = p / (2 * pi) * asin(c / a)
   end
 
   if t < 1 then
     t = t - 1
-    return -0.5 * (a * p(2, 10 * t) * s((t * d - s) * (2 * pi) / p)) + b
+    return -0.5 * (a * pow(2, 10 * t) * sin((t * d - s) * (2 * pi) / p)) + b
   else
     t = t - 1
-    return a * p(2, -10 * t) * s((t * d - s) * (2 * pi) / p) * 0.5 + c + b
+    return a * pow(2, -10 * t) * sin((t * d - s) * (2 * pi) / p ) * 0.5 + c + b
   end
 end
 
@@ -294,9 +315,9 @@ end
 -- p: period
 local function outInElastic(t, b, c, d, a, p)
   if t < d / 2 then
-    return outElastic(t * 2, b, c / 2, d)
+    return outElastic(t * 2, b, c / 2, d, a, p)
   else
-    return inElastic((t * 2) - d, b + c / 2, c / 2, d)
+    return inElastic((t * 2) - d, b + c / 2, c / 2, d, a, p)
   end
 end
 
@@ -348,7 +369,9 @@ local function outBounce(t, b, c, d)
   end
 end
 
-local function inBounce(t, b, c, d) return c - outBounce(d - t, 0, c, d) + b end
+local function inBounce(t, b, c, d)
+  return c - outBounce(d - t, 0, c, d) + b
+end
 
 local function inOutBounce(t, b, c, d)
   if t < d / 2 then
