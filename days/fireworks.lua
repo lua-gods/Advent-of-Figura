f58310a86130f18b7d55308cb1ca9fd0c62e94f0
GNamimates <81744903+GNamimates@users.noreply.github.com>
2023-11-13 18:45:27 +0800

    squish fireworks

--- days/fireworks.lua
+++ days/fireworks.lua
@@ -10,6 +10,41 @@ function day:init(skull)
     skull.data.offset = math.floor(skull.pos.x * 3 + skull.pos.z * 5 + skull.pos.y * 7) % 50
     skull.data.shift = vectors.vec3()
     skull.data.height = 0
+    skull.data.squish = 1
+end
+
+---@param skull Skull
+local function animation(skull)
+    tween.tweenFunction(skull.data.shift,vectors.vec3(math.random()-0.5,math.random()-0.5,(math.random()-0.5))*0.25,1,"outSine",function (x)
+        skull.data.shift = x
+        skull.data.part:setPos((skull.pos + vectors.vec3(x.x,skull.data.height,x.z)) * 16):setRot(0,x.y*180,0)
+    end)
+    tween.tweenFunction(skull.data.height,0.5,0.2,"outQuart",function (x) -- jump
+        skull.data.height = x --[[@as number]]
+    end,function ()
+        tween.tweenFunction(skull.data.height,0,0.2,"inSine",function (x) -- fall
+            skull.data.height = x --[[@as number]]
+        end)
+    end)
+    tween.tweenFunction(skull.data.squish,0.7,0.05,"outSine",function (x) -- squish 
+        skull.data.squish = x
+        skull.data.part:setScale(1/x,x,1/x)
+    end,function ()
+        tween.tweenFunction(skull.data.squish,1.4,0.2,"inOutQuad",function (x) -- squish 
+            skull.data.squish = x
+            skull.data.part:setScale(1/x,x,1/x)
+        end,function ()
+            tween.tweenFunction(skull.data.squish,.75,0.3,"inOutQuad",function (x) -- squish 
+                skull.data.squish = x
+                skull.data.part:setScale(1/x,x,1/x)
+            end,function ()
+                tween.tweenFunction(skull.data.squish,1,0.2,"inOutQuad",function (x) -- squish 
+                    skull.data.squish = x
+                    skull.data.part:setScale(1/x,x,1/x)
+                end)
+            end)
+        end)
+    end)
 end
 
 local function launch(skull)
@@ -18,6 +53,7 @@ local function launch(skull)
     for i = 1, 15 do
         particles["smoke"]:pos(skull.render_pos + vec(0.5, 0.5, 0.5)):velocity(rng.vec3().x_z--[[@as Vector3]] * 0.2 + vec(0,0.01,0)):gravity():scale(0.7):lifetime(rng.float(15,30)):spawn()
     end
+    animation(skull)
     delay(function()
         sounds["block.barrel.close"]:pos(skull.render_pos + vec(0.5, 0.5, 0.5)):pitch(1.5):volume(0.05):play()
         delay(function()
@@ -25,17 +61,7 @@ local function launch(skull)
             skull.data.part.lid:uvPixels(0,0)
         end, 4)
     end, 15)
-    tween.tweenFunction(skull.data.shift,vectors.vec3(math.random()-0.5,math.random()-0.5,(math.random()-0.5))*0.25,1,"outSine",function (x)
-        skull.data.shift = x
-        skull.data.part:setPos((skull.pos + vectors.vec3(x.x,skull.data.height,x.z)) * 16):setRot(0,x.y*180,0)
-    end)
-    tween.tweenFunction(skull.data.height,0.5,0.2,"outQuart",function (x)
-        skull.data.height = x --[[@as number]]
-    end,function ()
-        tween.tweenFunction(skull.data.height,0,0.5,"outBounce",function (x)
-            skull.data.height = x --[[@as number]]
-        end)
-    end)
+    
     skull.data.fireworks:spawn(skull.render_pos + vec(0.5, 0.5, 0.5), vec(0,1,0), variants[math.random(1, #variants)])
 end
 
