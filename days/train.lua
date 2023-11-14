5e87946e5865d9796a6dee8362b4c9b96a8faaf3
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2023-11-14 19:43:03 +0100

    more trainn

--- days/train.lua
+++ days/train.lua
@@ -1,9 +1,9 @@
 local Calendar = require("libraries.Calendar")
 local day = Calendar:newDay("train", 16) -- name and day number.
 
-local skulls = require("libraries.SkullManager").skulls
+local skullManager = require("libraries.SkullManager")
 
--- models.model.days.fallback.cube:setPrimaryTexture("RESOURCE", "missingno") -- do any pre-init stuff here.
+local trainSpeed = 0.1
 
 local facingToRot = {
    west = -90,
@@ -12,14 +12,51 @@ local facingToRot = {
    north = 180,
 }
 
-local trainCount = 1 -- change later
+local function getNextTrack(skull, startNode)
+   local nextPos = skull.pos + (startNode and skull.data.startNode or skull.data.endNode)
+   local newSkull = skullManager:get(nextPos)
+   if not (newSkull and newSkull.data.isTrain) then
+      nextPos.y = nextPos.y - 1
+      newSkull = skullManager:get(nextPos)
+   end
+   if newSkull and newSkull.data.isTrain then
+      local connectedEndPos = nextPos + newSkull.data.endNode
+      local backwards = connectedEndPos == skull.pos or connectedEndPos - vec(0, 1, 0) == skull.pos
+      local connectedStartPos = nextPos + newSkull.data.startNode
+      if backwards or connectedStartPos == skull.pos or connectedStartPos - vec(0, 1, 0) == skull.pos then
+         return newSkull, backwards
+      end
+   end
+end
+
+local function findTrainNearby(skull)
+   local pos = skull.pos
+   for direction = 0, 1 do
+      local startNode = direction == 1
+      local currentSkull
+      currentSkull, startNode = getNextTrack(skull, startNode)
+      for _ = 1, 16 do
+         if not currentSkull or currentSkull.pos == pos then
+            break
+         end
+         if currentSkull.data.train then
+            return skull
+         end
+
+         currentSkull, startNode = getNextTrack(currentSkull, startNode)
+      end
+   end
+end
+
 --- runs every time a skull is loaded.
 ---@param skull Skull
 function day:init(skull)
    skull.data.isTrain = true
+   -- find correct track type
    local block = world.getBlockState(skull.pos)
    local blockRot = block.properties.rotation or 0
-   if block.properties.facing then
+   if block.properties.facing then -- vertical
+      skull.data.trackType = 'vertical'
       local rot = facingToRot[block.properties.facing]
       skull.data.model = skull:addPart(models.train.up)
       skull.data.startNode = vec(0, 0, 1) * matrices.rotation3(0, rot, 0)
@@ -27,12 +64,14 @@ function day:init(skull)
       skull.data.rotOffset = rot
       skull.data.debugColor = vec(0, 1, 0)
    elseif blockRot % 4 == 0 then
+      skull.data.trackType = 'straight'
       skull.data.model = skull:addPart(models.train.straight)
       skull.data.startNode = blockRot % 8 == 0 and vec(0, 0, 1) or vec(1, 0, 0)
       skull.data.endNode = -skull.data.startNode
       skull.data.rotOffset = blockRot % 8 == 0 and 0 or 90
       skull.data.debugColor = vec(1, 0, 0)
    else
+      skull.data.trackType = 'rotated'
       skull.data.debugColor = vec(0, 0, 1)
       local rot = math.floor(blockRot / 4) * -90
       skull.data.model = skull:addPart(models.train.curve)
@@ -41,48 +80,37 @@ function day:init(skull)
       skull.data.endNode = vec(1, 0, 0) * mat
       skull.data.rotOffset = rot
    end
+   -- set rot and pos of model
+   skull.data.model:setRot(0, skull.data.rotOffset, 0)
+   skull.data.model:setPos(skull.pos * 16)
+   -- add train model
    skull.data.trainModel = skull:addPart(models.train.train):setVisible(false)
-   if trainCount >= 1 then  -- change later
-      trainCount = trainCount - 1
-      skull.data.train = {
-         oldTime = 0,
-         time = 0,
-         backwards = false,
-      }
-   end
-end
-
-local function getNextTrack(skull, startNode)
-   local nextPos = skull.pos + (startNode and skull.data.startNode or skull.data.endNode)
-   local newSkull = skulls[tostring(nextPos)]
-   if not (newSkull and newSkull.data.isTrain) then
-      nextPos.y = nextPos.y - 1
-      newSkull = skulls[tostring(nextPos)]
-   end
-   if newSkull and not newSkull.data.train then
-      local connectedEndPos = nextPos + newSkull.data.endNode
-      local backwards = connectedEndPos == skull.pos or connectedEndPos - vec(0, 1, 0) == skull.pos
-      return newSkull, backwards
-   end
+   skull.data.spawnTrainTime = 3
 end
 
 --- every world tick.
 ---@param skull Skull
 function day:tick(skull)
-   ---[[ -- debug overlay
-   if (({ client:getViewer():getTargetedBlock() })[2] or vec(0, 0)):floor() == skull.pos then
-      for _ = 1, 10 do
-         particles["end_rod"]:pos(skull.pos + vec(math.random(), math.random(), math.random()) +
-         skull.data.startNode):color(skull.data.debugColor):lifetime(8):spawn()
-         particles["end_rod"]:pos(skull.pos + vec(math.random(), math.random(), math.random()) +
-         skull.data.endNode):color(skull.data.debugColor):lifetime(8):spawn()
+   -- spawn train
+   skull.data.spawnTrainTime = math.max(skull.data.spawnTrainTime - 1, 0)
+   if skull.data.spawnTrainTime == 1 then
+      -- check if train already exists on track
+      -- spawn train if it doesnt exist on track already
+      if not findTrainNearby(skull) then
+         skull.data.train = {
+            oldTime = 0,
+            time = 0,
+            backwards = false,
+            speed = trainSpeed
+         }
       end
    end
-   --]]
+   -- move into another skull
    local train = skull.data.train
-   if train then  -- add moving into another head
+   if train then
       train.oldTime = train.time
-      train.time = train.time + 0.15
+      train.time = train.time + train.speed
+      train.speed = math.lerp(train.speed, trainSpeed, 0.1)
       if train.time >= 1 then
          train.oldTime = train.oldTime % 1 - 1
          train.time = train.time % 1
@@ -95,8 +123,9 @@ function day:tick(skull)
             -- turn backwards
             newSkull, backwards = getNextTrack(skull, not train.backwards)
             if newSkull then
-               train.oldTime = train.oldTime % 1 - 1
-               train.time = train.time % 1
+               train.oldTime = 0
+               train.time = 0
+               train.speed = 0
                train.backwards = not train.backwards
             end
          end
@@ -112,19 +141,35 @@ end
 
 ---@param skull Skull
 ---@param delta number
-function day:render(skull, delta) -- every world render.
-   skull.data.model:setRot(0, skull.data.rotOffset, 0)
-   skull.data.model:setPos(skull.pos * 16)
-
+function day:render(skull, delta)
+   -- render train
    local train = skull.data.train
    local trainModel = skull.data.trainModel
    if train then
       trainModel:setVisible(true)
       local trainTime = math.lerp(train.oldTime, train.time, delta)
-      skull.data.trainModel.start:setPos(0, 0, trainTime * -16)
-      -- temp train rendering
-      trainModel:setRot(0, skull.data.rotOffset + (train.backwards and 180 or 0), 0)
       skull.data.model:setColor(0.5, 0.5, 0.5)
+      local backwards = train.backwards
+      local rotOffset = skull.data.rotOffset
+      if skull.data.trackType == 'straight' then
+         trainModel.start:setPos(0, 0, trainTime * -16)
+         trainModel:setRot(0, rotOffset + (backwards and 180 or 0), 0)
+      elseif skull.data.trackType == 'rotated' then
+         local rot = trainTime * 90
+         trainModel:setRot(0, rotOffset, 0)
+         trainModel.start:setPos(0, 0, 0)
+         if backwards then
+            trainModel:setPos(skull.pos * 16 + vec(16, 0, 0) * matrices.rotation3(0, rotOffset, 0))
+            trainModel.start:setPivot(-8, 0, 8)
+            trainModel.start:setRot(0, rot + 90, 0)
+         else
+            trainModel:setPos(skull.pos * 16)
+            trainModel.start:setPivot(8, 0, 8)
+            trainModel.start:setRot(0, -rot, 0)
+         end
+      elseif skull.data.trackType == 'vertical' then
+
+      end
    else
       trainModel:setVisible(false)
       skull.data.model:setColor(1, 1, 1)
@@ -134,5 +179,20 @@ end
 ---called when the skull is destroyed, unloaded, or switched to a different day. Do cleanup here.
 ---@param skull Skull
 function day:exit(skull)
-   --  skull.data.flash:remove() -- we'll remove the flash particle we created in init.
+   local train = skull.data.train
+   if train then
+      if not findTrainNearby(skull) then
+         local newSkull, backwards = getNextTrack(skull, train.backwards)
+         if newSkull then
+            train.backwards = backwards
+            newSkull.data.train = train
+         else
+            newSkull, backwards = getNextTrack(skull, not train.backwards)
+            if newSkull then
+               train.backwards = backwards
+               newSkull.data.train = train
+            end
+         end
+      end
+   end
 end
