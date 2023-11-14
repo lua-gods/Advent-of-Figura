8fbbf6eba77b804437363e391907aab00a258986
4P5 <theprismarinescientist@gmail.com>
2023-11-14 23:02:26 +1300

    More random
    - Added rng.int and rng.bool
    - rng.step, which generates a number in steps
    - rng.of, which takes a table and returns one of its contents, or any number of varargs and returns of of them

--- libraries/random.lua
+++ libraries/random.lua
@@ -12,4 +12,28 @@ function rng.float(min, max)
     return math.random() * (max - min) + min
 end
 
+---@param ... number m, n
+---@return integer
+function rng.int(...)
+    return math.floor(math.random(...))
+end
+
+---@return boolean
+function rng.bool()
+    return math.random() > 0.5
+end
+
+---@param from number
+---@param to number
+---@param step number
+function rng.step(from, to, step)
+    return math.floor((math.random() * (to - from) + from) / step) * step
+end
+
+---@param ... table|...
+---@return any
+function rng.of(...)
+    return type((...)) == "table" and (...)[rng.int(#(...))] or rng.of({...})
+end
+
 _G.rng = rng
