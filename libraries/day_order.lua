f99d4f7692a0a1f093adaf7de8561bf962a51b7e
4P5 <theprismarinescientist@gmail.com>
2023-11-15 22:05:59 +1300

    Personal space
    - "back away slowly with your hands in the air... don't make yourself a threat..."

--- libraries/day_order.lua
+++ libraries/day_order.lua
@@ -3,14 +3,14 @@ return {
     nil,
     "jukebox", -- 4P5
     "train", -- Auria
-    nil,
     "snowman", -- 4P5
     "door_wreath", -- Chloe
+    nil,
+    nil, -- NO TOUCHY
     nil,
     "baubles", -- 4P5
     "snowflakes", -- 4P5
     "snowball_gun", -- 4P5
-    nil,
     nil,
     nil,
     "fireworks", -- 4P5
