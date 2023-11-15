8606bfb0c44e3306c401e120e55262b422ee6636
ChloeSpacedOut <chloespacedout@gmail.com>
2023-11-15 14:38:55 +1000

    wreath tweaks
    added inwards facing orientation, and made the wreath snap to NSEW orientation

--- days/door_wreath.lua
+++ days/door_wreath.lua
@@ -14,6 +14,8 @@ function toggleDoorHoligram(skull,toggle)
   else
     skull.data.door_wreath.door:setVisible(false)
     skull.data.door_wreath.block:setVisible(false)
+    skull.data.door_wreath.wreath.open:setRot(0,0,0)
+    skull.data.door_wreath.wreath.open:setPos(0,0,0)
   end
 end
 
@@ -22,6 +24,7 @@ function day:init(skull)
   skull.data.door_wreath = skull:addPart(models.door_wreath)
   if not skull.is_wall_head then
     toggleDoorHoligram(skull,true)
+    skull.data.door_wreath:setRot(0,math.floor(-skull.rot/90)*90,0)
   end
 
 end
@@ -31,21 +34,9 @@ function day:tick(skull)
   if skull.is_wall_head then return end
   skull.data.door_wreath:setLight(world.getLightLevel(skull.pos + vec(0,2,0)))
   local door = world.getBlockState(skull.pos + vec(0,2,0))
-  if string.find(door.id,"door") then
+  local properties = door:getProperties()
+  if string.find(door.id,"door") and not string.find(door.id,"trap") then
     toggleDoorHoligram(skull,false)
-    local properties = door:getProperties()
-    if properties.hinge == "right" then
-      skull.data.door_wreath.wreath:setScale(-1,1,1)
-    else
-      skull.data.door_wreath.wreath:setScale(1,1,1)
-    end
-    if properties.open == "true" then
-      skull.data.door_wreath.wreath.open:setRot(0,90,0)
-      skull.data.door_wreath.wreath.open:setPos(-4,0,0)
-    else
-      skull.data.door_wreath.wreath.open:setRot(0,0,0)
-      skull.data.door_wreath.wreath.open:setPos(0,0,0)
-    end
   else
     toggleDoorHoligram(skull,true)
   end
@@ -59,7 +50,51 @@ end
 ---@param skull Skull
 ---@param delta number
 function day:render(skull, delta)
-  skull.data.door_wreath.door:setOpacity(math.sin((world.getTime()+delta)/5)/4+0.75)
+  if skull.is_wall_head then return end
+  
+  local door = world.getBlockState(skull.pos + vec(0,2,0))
+  if string.find(door.id,"door") and not string.find(door.id,"trap") then
+    local properties = door:getProperties()
+    local mod = 0
+    -- tried to do this with vectors. Didn't work. Got mad. Here's your dumb hardcoded solution, I don't even care anymore ;w;
+    local headDir = -(math.floor(-skull.rot/90)*90)
+    if headDir == 360 then
+      headDir = 0
+    end
+    local NS = headDir == 0 and properties.facing == "north"
+    local WE = headDir == 270 and properties.facing == "west"
+    local SN = headDir == 180 and properties.facing == "south"
+    local EW = headDir == 90 and properties.facing == "east"
+    local backsideReef = (NS or WE or SN or EW)
+    if properties.hinge == "right" then
+      mod = 1
+    else
+      mod = -1
+    end
+    if properties.open == "true" then
+      if not backsideReef then
+        skull.data.door_wreath.wreath.open:setPivot(-9*mod,4,3)
+        skull.data.door_wreath.wreath.open:setRot(0,-90*mod,0)
+        skull.data.door_wreath.wreath.open:setPos(4*mod,0,0)
+      else
+        skull.data.door_wreath.wreath.open:setPivot(-9*mod,4,16)
+        skull.data.door_wreath.wreath.open:setRot(0,90*mod,0)
+        skull.data.door_wreath.wreath.open:setPos(26*mod,0,5)
+      end
+    else
+      if not backsideReef then
+        skull.data.door_wreath.wreath.open:setPivot(-9*mod,4,3)
+        skull.data.door_wreath.wreath.open:setRot(0,0,0)
+        skull.data.door_wreath.wreath.open:setPos(0,0,0)
+      else
+        skull.data.door_wreath.wreath.open:setPivot(-9*mod,4,16)
+        skull.data.door_wreath.wreath.open:setRot(0,0,0)
+        skull.data.door_wreath.wreath.open:setPos(0,0,13)
+      end
+    end
+  else
+    skull.data.door_wreath.door:setOpacity(math.sin((TIME+delta)/5)/4+0.75)
+  end
 end
 
 ---@param skull Skull
