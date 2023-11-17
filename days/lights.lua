8a49d1d249255d0667c6a121296b0c1f47824110
4P5 <theprismarinescientist@gmail.com>
2023-11-17 15:41:25 +1300

    Lights
    - Uses splines to make christmas lights
    - Not happy with these at the moment

---
+++ days/lights.lua
@@ -0,0 +1,30 @@
+local Calendar = require("libraries.Calendar")
+local SplineManager = require("libraries.SplineManager")
+local tween = require("libraries.GNTweenLib")
+
+---@class Day.Lights: Day
+---@field manager SplineManager
+local day = Calendar:newDay("baubles")
+
+local manager = SplineManager.new(models.lights.Light:getChildren())
+
+function day:globalTick()
+    manager:tick()
+end
+
+function day:init(skull)
+    manager.needs_pairing = true
+    manager:addSkull(skull)
+end
+
+function manager.postPartRender(part)
+    part:color(vectors.hsvToRGB(rng.float(0,1),1,1)):primaryRenderType("EMISSIVE_SOLID")
+end
+
+function manager.postSkullRender(skull)
+
+end
+
+function day:exit(skull)
+    manager:exit(skull)
+end
