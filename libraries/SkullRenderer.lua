d9f1290398ee35b4355584848d0311fb2ba13ad5
4P5 <4P5@stormheart.net>
2023-11-19 03:24:58 +1300

    Entity rendering
    - If no blocks are available to use for rendering, entities wearing the skull will be used
    - There's currently a bug with a slightly incorrect offset and rotation interpolation

--- libraries/SkullRenderer.lua
+++ libraries/SkullRenderer.lua
@@ -15,17 +15,22 @@ local facing_offsets = {
 }
 
 local render_skull = false
+local fallback = 0
 function events.WORLD_RENDER()
     render_skull = true
+    fallback = fallback + 1
 end
 
+local ENTITY_OFFSET = vec(7,-19.5,7)
+local PLAYER_SCALE = math.playerScale ^ 2
 local item = models.model.item:setParentType("SKULL")
-function events.SKULL_RENDER(_, blockstate)
+function events.SKULL_RENDER(delta, blockstate, itemstack, entity, context)
+    item:visible(false)
     if blockstate then
-        item:visible(false)
-        local pos = blockstate:getPos()
+        fallback = 0
         if render_skull then
             render_skull = false
+            local pos = blockstate:getPos()
             if blockstate.id == "minecraft:player_head" then
                 world_base:rot(0,blockstate.properties.rotation*22.5,0):visible(true)
                 world_part:pos(-pos*16)
@@ -33,6 +38,16 @@ function events.SKULL_RENDER(_, blockstate)
                 world_base:rot(0,facing_rots[blockstate.properties.facing]*22.5,0):visible(true)
                 world_part:pos((-pos + facing_offsets[blockstate.properties.facing])*16)
             end
+        else
+            world_base:visible(false)
+        end
+    elseif entity and context == "HEAD" then
+        if render_skull and fallback > 1 then
+            render_skull = false
+            world_base:visible(true)
+            local rot = entity:getRot(delta)
+            world_base:matrix(matrices.mat4():translate(-entity:getPos(delta) * 16):scale(PLAYER_SCALE, PLAYER_SCALE, PLAYER_SCALE):translate(ENTITY_OFFSET):rotate(0, rot.y + 180, 0):rotate(rot.x, 0, 0))
+            world_part:pos()
         else
             world_base:visible(false)
         end
