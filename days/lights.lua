0db05cb794f5513b715f3a9712cee0381d3750e5
ChloeSpacedOut <chloespacedout@gmail.com>
2023-12-01 16:48:29 +1000

    Revamped Lights
    Improved the looks of the lights

--- days/lights.lua
+++ days/lights.lua
@@ -8,7 +8,7 @@ local day = Calendar:newDay("lights")
 
 local manager = SplineManager.new(models.lights.Light:getChildren())
 
-function day:globalTick()
+function day:globalTick(skull)
     manager:tick()
 end
 
@@ -16,9 +16,13 @@ function day:init(skull)
     manager.needs_pairing = true
     manager:addSkull(skull)
 end
-
+local colors = {vec(0.8,0,0),vec(0,0.6,0.2),vec(0,0.1,0.8),vec(0.95,0.65,0)}
+local colorCount = 0
 function manager.postPartRender(part)
-    part:color(vectors.hsvToRGB(rng.float(0,1),1,1)):primaryRenderType("EMISSIVE_SOLID")
+    colorCount = (colorCount + 1 ) % 4
+    part.bulb:setColor(colors[colorCount + 1]):setRot(-part:getRot().x,rng.float(-35,35),0)
+    part:primaryRenderType("EMISSIVE_SOLID")
+    part.bulb.bulbGlass:setPrimaryRenderType("EMISSIVE"):setOpacity(0.5)
 end
 
 function manager.postSkullRender(skull)
