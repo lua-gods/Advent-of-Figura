d7a38902d7bd5c93a5dc2a736795ce6629e32a0f
4P5 <theprismarinescientist@gmail.com>
2023-12-01 16:12:21 +1300

    Present system
    - Simple system for presents, with random effects based on the world coords

---
+++ days/present.lua
@@ -0,0 +1,62 @@
+local Calendar = require("libraries.Calendar")
+local FireworkManager = require("libraries.fireworks.FireworkManager")
+local variants = require("libraries.fireworks.variants")
+
+local day = Calendar:newDay("present")
+
+local effects = {
+    function(skull)
+        for i = 1, 20 do
+            particles["end_rod"]:pos(skull.render_pos + vec(0.5,0.5,0.5)):velocity(rng.vec3():add(0,1,0):scale(0.1)):color(vectors.hsvToRGB(rng.float(0,1),0.5,1)):spawn()
+        end
+        sounds["block.note_block.bell"]:pos(skull.render_pos + vec(0.5,0.5,0.5)):pitch(rng.float(0.5,1.5)):volume(0.5):play()
+    end,
+    function(skull)
+        skull.data.fireworks:spawn(skull.render_pos + vec(0.5,0.5,0.5), rng.vec3():add(0,1,0):scale(0.5), rng.of(variants))
+    end,
+}
+
+---@param skull Skull
+function day:init(skull)
+    skull.data.fireworks = FireworkManager.new()
+    math.randomseed(skull.pos.x * 3 + skull.pos.z * 5 + skull.pos.y * 7)
+    math.random(); math.random(); math.random()
+    skull.data.effect = rng.of(effects)
+end
+
+---@param skull Skull
+function day:tick(skull)
+    skull.data.fireworks:tick()
+end
+
+---@param skull Skull
+---@param puncher Player
+function day:punch(skull,puncher)
+    skull.data.effect(skull)
+end
+
+---@param skull Skull
+---@param delta number
+function day:render(skull, delta)
+
+end
+
+---@param skull Skull
+function day:exit(skull)
+
+end
+
+---@param skulls Skull[]
+function day:globalTick(skulls)
+
+end
+
+---@param skulls Skull[]
+function day:globalInit(skulls)
+
+end
+
+---@param skulls Skull[]
+function day:globalExit(skulls)
+
+end
