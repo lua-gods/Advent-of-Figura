ec5b446918818f8cea3e25119cecfd89ce42a2ec
ChloeSpacedOut <chloespacedout@gmail.com>
2023-12-01 18:43:26 +1000

    Blinking lights
    - made the lights blink
    - added still colour version with redstone input

--- days/lights.lua
+++ days/lights.lua
@@ -7,17 +7,48 @@ local tween = require("libraries.GNTweenLib")
 local day = Calendar:newDay("lights")
 
 local manager = SplineManager.new(models.lights.Light:getChildren())
-
+local toggle = false
+local colors = {vec(0.8,0,0),vec(0,0.1,0.8),vec(0.95,0.65,0),vec(0,0.6,0.2),}
+local colorCount = 0
+local isPowered = false
 function day:globalTick(skull)
     manager:tick()
+    colorCount = 0
+    toggle = not toggle
+        if TIME % 20 == 0 then
+            if isPowered then
+                for key,part in pairs(manager.parts) do
+                    colorCount = (colorCount + 1 ) % 4
+                    part.bulb:setColor(colors[colorCount + 1])
+                end
+            else
+                for key,part in pairs(manager.parts) do
+                    toggle = not toggle
+                    colorCount = (colorCount + 1 ) % 4
+                    if toggle then
+                        part.bulb:setColor(colors[colorCount + 1])
+                    else
+                        part.bulb:setColor(colors[colorCount + 1]/8)
+                    end
+        
+                end
+            end
+        end
+    isPowered = false
 end
+
+function day:tick(skull)
+    if world.getRedstonePower(skull.pos) ~= 0 then
+        isPowered = true
+    end
+end
+
 
 function day:init(skull)
     manager.needs_pairing = true
     manager:addSkull(skull)
 end
-local colors = {vec(0.8,0,0),vec(0,0.6,0.2),vec(0,0.1,0.8),vec(0.95,0.65,0)}
-local colorCount = 0
+
 function manager.postPartRender(part)
     colorCount = (colorCount + 1 ) % 4
     part.bulb:setColor(colors[colorCount + 1]):setRot(-part:getRot().x,rng.float(-35,35),0)
