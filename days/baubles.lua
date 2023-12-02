b4de3cfa404d6a4447743bfca6fac7f432f1e12d
4P5 <theprismarinescientist@gmail.com>
2023-12-02 17:10:56 +1300

    New baubles
    - Baubles now use swingables to do swinging physics
    - Bug with startup currently

--- days/baubles.lua
+++ days/baubles.lua
@@ -1,6 +1,6 @@
 local Calendar = require("libraries.Calendar")
-local SplineManager = require("libraries.SplineManager")
-local tween = require("libraries.GNTweenLib")
+local Swingable3D = require("libraries.Swingable3D")
+local deepCopy = require("libraries.deep_copy")
 
 ---@class Day.Baubles: Day
 ---@field manager SplineManager
@@ -14,25 +14,55 @@ function day:wornRender()
 end
 
 local baubles = models.baubles.Bauble:getChildren()
-local manager = SplineManager.new(models.baubles.Vine:getChildren())
 
 function day:globalTick()
-    manager:tick()
     item_part:rot(0, TIME * 0.5, 0):color(vectors.hsvToRGB((client:getSystemTime() / 5000) % 1,0.8,1))
 end
 
-function day:init(skull)
-    manager.needs_pairing = true
-    manager:addSkull(skull)
-    
-    math.randomseed(skull.pos.x * 73856093 + skull.pos.y * 19349663 + skull.pos.z * 83492791)
-    math.random(); math.random(); math.random()
+local function findAnchor(pos)
+    for i = 1, 20 do
+        if not world.getBlockState(pos + vec(0,i,0)):isAir() then
+            return pos + vec(0,i,0)
+        end
+    end
+    return pos + vec(0,1,0)
 end
 
-function manager.postSkullRender(skull)
-    skull:addPart(baubles[math.random(1, #baubles)]):color(vectors.hsvToRGB(rng.float(0,1),1,1))
+function day:init(skull)
+    math.randomseed(skull.pos.x * 73856093 + skull.pos.y * 19349663 + skull.pos.z * 83492791)
+    math.random(); math.random(); math.random()
+
+    local part = skull:addPart(models:newPart("bauble_anchor"))
+
+    local anchor = findAnchor(skull.pos)
+    local offset = skull.pos - anchor
+    part:pos((skull.render_pos - offset) * 16)
+
+    local bauble = deepCopy(baubles[math.random(1, #baubles)]):color(vectors.hsvToRGB(rng.float(0,1),1,1))
+    bauble:pos(0,offset.y * 16,0)
+    bauble.string:scale(1, -offset.y * 16 - 8, 1)
+    part:addChild(bauble)
+
+    local swingable = Swingable3D.new(part)
+    skull.data.swingable = swingable
+    skull.data.wind = vec(0,0,0)
+end
+
+function day:tick(skull)
+    if not skull.data.swingable then return end
+    skull.data.wind = skull.data.wind * 0.8
+    skull.data.swingable:tick(skull.data.wind)
+end
+
+function day:punch(skull, puncher)
+    skull.data.wind = skull.data.wind - (puncher:getPos() - skull.pos):normalize() * 0.1
+end
+
+function day:render(skull, delta)
+    if not skull.data.swingable then return end
+    skull.data.swingable:render(delta)
 end
 
 function day:exit(skull)
-    manager:exit(skull)
+
 end
