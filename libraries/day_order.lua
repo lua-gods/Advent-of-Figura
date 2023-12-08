c6a51155d2ec4678430848fe9c357686cb1d6703
GNamimates <81744903+GNamimates@users.noreply.github.com>
2023-12-08 09:02:11 +0800

    line = day

--- libraries/day_order.lua
+++ libraries/day_order.lua
@@ -1,26 +1,25 @@
-return {
-    "snowman", -- 4P5
-    "snowfall", -- Chloe
-    "fireworks", -- 4P5
-    "train", -- Auria
-    "jukebox", -- 4P5
-    "door_wreath", -- Chloe
-    "christmas_hat", -- Auria
-    "snow_globe", -- 4P5
-    "cauldron", -- juice of apple
-    "vines", -- 4P5
-    "snowflakes", -- 4P5
-    "shelfElf", -- Chloe
-    "tree", -- GN & Slyme
-    "lights", -- 4P5 & Chloe
-    "baubles", -- 4P5
-    nil,
-    nil,
-    nil,
-    nil,
-    nil,
-    nil,
-    nil,
-    nil,
-    "badge" -- 4P5
+return {"snowman", -- 4P5
+        "snowfall", -- Chloe
+        "fireworks", -- 4P5
+        "train", -- Auria
+        "jukebox", -- 4P5
+        "door_wreath", -- Chloe
+        "christmas_hat", -- Auria
+        "snow_globe", -- 4P5
+        "cauldron", -- juice of apple
+        "vines", -- 4P5
+        "snowflakes", -- 4P5
+        "shelfElf", -- Chloe
+        "tree", -- GN & Slyme
+        "lights", -- 4P5 & Chloe
+        "baubles", -- 4P5
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+        "badge" -- 4P5
 }
