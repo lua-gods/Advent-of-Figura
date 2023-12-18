f97bf125e3cb81e8a11e53b92637c04a425fc770
4P5 <theprismarinescientist@gmail.com>
2023-12-19 03:18:27 +1300

    Pufferfish bubbles
    - Replace bubbles with a pufferfish

--- days/bubbles.lua
+++ days/bubbles.lua
@@ -5,14 +5,15 @@ local Bubble = require("libraries.Bubble")
 ---@param skull Skull
 function day:init(skull)
     skull.data.overdrive = 0
-    skull.data.part = skull:addPart(models.bubbles.Machine)
+    skull.data.part = skull:addPart(models.bubbles.Puff):scale(0.75)
+    skull:addPart(models.bubbles.Bowl)
 end
 
 ---@param skull Skull
 function day:tick(skull)
-    local pos = skull.pos + vec(0.5,0.5,0.5) + vec(0,0.3,0) + vec(math.sin((TIME + 2)/10),0,math.cos((TIME + 2)/10)) * 0.5
+    local pos = skull.pos + vec(0.5,0.5,0.5) + vec(0,0.025,0) - vec(math.sin((TIME + 2)/10),0,math.cos((TIME + 2)/10)) * 0.2
     if math.random() > 0.6 then
-        Bubble.new(pos, vec(0,0.1,0) + vec(math.sin(TIME/10),0,math.cos(TIME/10)) * 0.12)
+        Bubble.new(pos, vec(0,0.025,0) - vec(math.sin(TIME/10),0,math.cos(TIME/10)) * 0.12)
         for _ = 1, 3 do
             particles["bubble_pop"]:pos(pos):scale(1.2):spawn()
             particles["splash"]:pos(pos + rng.vec3() * 0.1):scale(0.5):spawn()
@@ -20,7 +21,7 @@ function day:tick(skull)
     end
     if skull.data.overdrive > 0 then
         for i = 1, 5 do
-            Bubble.new(pos, vec(0,0.1,0) + rng.vec3():normalize() * 0.15 + vec(math.sin(TIME/10),0,math.cos(TIME/10)) * 0.1)
+            Bubble.new(pos, vec(0,0.025,0) + rng.vec3():normalize() * 0.15 + vec(math.sin(TIME/10),0,math.cos(TIME/10)) * 0.1)
             for _ = 1, 2 do
                 particles["bubble_pop"]:pos(pos):scale(1.2):spawn()
                 particles["splash"]:pos(pos + rng.vec3() * 0.1):scale(0.5):spawn()
@@ -39,7 +40,8 @@ end
 ---@param skull Skull
 ---@param delta number
 function day:render(skull, delta)
-    skull.data.part.Gun:rot(utils.dirToAngle(vec(math.sin((TIME + delta)/10),0.6,math.cos((TIME + delta)/10))) + vec(0,skull.rot,0))
+    skull.data.part:rot(utils.dirToAngle(vec(math.sin((TIME + delta)/10),-0.5,math.cos((TIME + delta)/10))))
+    :pos((skull.render_pos + vec(0,math.sin((TIME + delta) / 15) * 0.05 + 0.1,0)) * 16)
 end
 
 ---@param skull Skull
